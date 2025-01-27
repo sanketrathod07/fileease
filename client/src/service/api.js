38e921dd0a752c800a56115a33eb628814898903
Sanket Rathod <118626260+sanketrathod07@users.noreply.github.com>
2025-01-27 23:39:04 +0530

    Update api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const uploadFile = async (data) => {
     try {
-        const response = await axios.post(`${process.env.API_URI || 'https://fileease-server.onrender.com'}/upload`, data);
+        const response = await axios.post('https://fileease-server.onrender.com/upload`, data);
         return response.data;
     } catch (error) {
         console.log('Error while calling the API ', error.message);
